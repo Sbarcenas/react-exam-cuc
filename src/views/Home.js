431000504f5d5c6ce74502c8f636448132fb91bd
Sebastian Barcenas <sbarcenas00@gmail.com>
2021-04-23 20:03:05 -0500

    Copyrigth

--- src/views/Home.js
+++ src/views/Home.js
@@ -55,23 +55,23 @@ Card.Body = styled(Paragraph)`
 const DATA = [
     {
         title: "Today's workout plan",
-        text: "We're gonna do 3 fundamentals exercises"
+        text: "We're gonna do 3 fundamentals exercises."
     },
     {
-        title: "Today's workout plan",
-        text: "We're gonna do 3 fundamentals exercises"
+        title: "First, 10 push-ups",
+        text: "Do 10 reps. Remember about full range of motion. Don't rush."
     },
     {
-        title: "Today's workout plan",
-        text: "We're gonna do 3 fundamentals exercises"
+        title: "Next 20 squads",
+        text: "Squads are important. Remember to keep your back straight."
     },
     {
-        title: "Today's workout plan",
-        text: "We're gonna do 3 fundamentals exercises"
+        title: "Finally, 15 sit-ups",
+        text: "Slightly bend your knees. Remember about full range of motion."
     },
     {
-        title: "Today's workout plan",
-        text: "We're gonna do 3 fundamentals exercises"
+        title: "Great job!",
+        text: "You made it, have a nice day and see you next time!"
     }
 ]
 
